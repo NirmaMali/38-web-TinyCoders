81a22b6c8faec49c9d7b755eb595ec50560f699c
Nirma Mali <nirmamali8618@gmail.com>
2026-06-18 13:20:03 +0530

    updated version

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+console.log('⏳ Starting PlaceIQ server...');
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
